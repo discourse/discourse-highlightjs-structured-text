05352de4e2278284215a16f643d9649d6972d9d3
Italo Alves <italo@discourse.org>
2023-10-20 12:25:29 -0300

    fix code format

--- javascripts/discourse/initializers/iecst.js
+++ javascripts/discourse/initializers/iecst.js
@@ -3,7 +3,7 @@ import iecst from "../lib/iecst-highlightjs";
 
 export default {
   name: "highlightjs-structured-text",
-  initialize(container) {
+  initialize() {
     withPluginApi("1.4.0", (api) => {
       api.registerHighlightJSLanguage("structured-text", iecst);
     });
